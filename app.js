9adb62f1853b24b19be658d7819e2ed648c3a8fd
debu1207 <patradebu1207@gmail.com>
2023-08-29 20:06:54 +0530

    Adding modules and app files

---
+++ app.js
@@ -0,0 +1,20 @@
+// Globals - no window 
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about env where the progream is being executed
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(module);
+// console.log(process);
+// const sayHiModule = require('./modules');
+// const kuchbhi = require('./alternative-syntax');
+// console.log(kuchbhi);
+// setInterval(()=>{
+//     sayHiModule.sayHi("debu");
+// }, 500)
+
+require('./mind-grenade')
