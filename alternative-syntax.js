9b9923ad584bb5240fbf144e5229ca7a8f0535ea
debu1207 <patradebu1207@gmail.com>
2023-08-29 20:08:03 +0530

    alternative syntax to export

---
+++ alternative-syntax.js
@@ -0,0 +1,8 @@
+const items = ['apple', 'paneer', 'potato'];
+
+const person = {
+    name: 'bob',
+    age: 34,
+};
+
+module.exports = {items, person};
---
+++ mind-grenade.js
@@ -0,0 +1,8 @@
+const a = 10;
+const b = 222;
+
+function addNumbers() {
+    console.log(`Sum of numbers is ${a + b}`);
+}
+
+addNumbers()
